0f21feba9bd6db46cc5f3f6ba15288fa2b3061b5
alexbreamdev <kortes39dev@gmail.com>
2022-09-09 22:07:48 +0300

    create generic onboarding view controller
    This commit add a generic onboarding view controller that can be reused across
    multiple onboarding screens.

--- Bankey/Bankey/OnboardingViewController.swift
+++ Bankey/Bankey/OnboardingViewController.swift
@@ -12,16 +12,29 @@ class OnboardingViewController: UIViewController {
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
-
+    var heroImageName: String
+    var titleText: String
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        style()
+        style(heroImageName: heroImageName, titleText: titleText)
         layout()
     }
 }
 
 extension OnboardingViewController {
-    func style() {
+    func style(heroImageName: String, titleText: String) {
+        view.backgroundColor = .systemBackground
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -29,14 +42,14 @@ extension OnboardingViewController {
         // image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         // label
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is fastest, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
         label.font = UIFont.preferredFont(forTextStyle: .title3)
     }
     
